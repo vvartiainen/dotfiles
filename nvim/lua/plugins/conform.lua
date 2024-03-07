36e255b81a2b6bf8f8984b6e55adc1ded6334f8a
Ville Vartiainen <vvarti@gmail.com>
2024-03-07 21:24:48 +0200

    conform config

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,59 @@
+local function biome_lsp_or_prettier(bufnr)
+  local has_biome_lsp = vim.lsp.get_active_clients({
+    bufnr = bufnr,
+    name = "biome",
+  })[1]
+  if has_biome_lsp then
+    return {}
+  end
+  local has_prettier = vim.fs.find({
+    -- https://prettier.io/docs/en/configuration.html
+    ".prettierrc",
+    ".prettierrc.json",
+    ".prettierrc.yml",
+    ".prettierrc.yaml",
+    ".prettierrc.json5",
+    ".prettierrc.js",
+    ".prettierrc.cjs",
+    ".prettierrc.toml",
+    "prettier.config.js",
+    "prettier.config.cjs",
+  }, { upward = true })[1]
+  if has_prettier then
+    return { "prettier" }
+  end
+  return { "biome" }
+end
+
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        lua = { "lua-format" },
+        javascript = biome_lsp_or_prettier,
+        typescript = biome_lsp_or_prettier,
+        json = biome_lsp_or_prettier,
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        html = { "prettier" },
+        css = { "prettier" },
+        scss = { "prettier" },
+        sass = { "prettier" },
+        less = { "prettier" },
+        rust = { "rustfmt" },
+        go = { "gofmt" },
+        shell = { "shfmt" },
+        vim = { "vimfmt" },
+        sql = { "sqlformat" },
+        dockerfile = { "dockerfilefmt" },
+        toml = { "tomllint" },
+        xml = { "xmllint" },
+        hcl = { "hclfmt" },
+        terraform = { "terraform fmt" },
+        tf = { "terraform fmt" },
+        tfvars = { "terraform fmt" },
+      },
+    },
+  },
+}
