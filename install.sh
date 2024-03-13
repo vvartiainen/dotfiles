2be5db0917cfd872625d7a976154a0fce4356c46
Ville Vartiainen <12277119+vvartiainen@users.noreply.github.com>
2024-03-13 09:26:14 +0200

    add tmux config

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/wezterm ~/.config/wezterm
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/tmux ~/.config/tmux
 
 # Add .zsh_profile
 echo "source ~/dotfiles/zsh/.zsh_profile" >>~/.zsh_profile
