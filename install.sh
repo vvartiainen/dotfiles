c4dc5dbc90942555811f7796de8b8fb5df18ffe9
Ville Vartiainen <vvarti@gmail.com>
2024-03-15 18:04:46 +0200

    fix tmux conf install

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # Symlink dotfiles
-ln -s ~/dotfiles/nvim ~/.config/nvim
-ln -s ~/dotfiles/wezterm ~/.config/wezterm
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/prog/dotfiles/nvim ~/.config/nvim
+ln -s ~/prog/dotfiles/wezterm ~/.config/wezterm
+ln -s ~/prog/dotfiles/.gitconfig ~/.gitconfig
 
-ln -s ~/dotfiles/tmux ~/.config/tmux
-ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/prog/dotfiles/tmux/tmux.conf ~/.tmux.conf
 
 # Add .zsh_profile
 echo "source ~/dotfiles/zsh/.zsh_profile" >>~/.zsh_profile
