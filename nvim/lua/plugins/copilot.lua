48c6374878536e9e7360862a0e5b14ab7bd58773
Ville Vartiainen <vvarti@gmail.com>
2024-03-17 17:18:22 +0200

    add copilot definitions

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "zbinw/copilot.lua",
+    opts = {
+      filetypes = {
+        javascript = true,
+        typescript = true,
+        go = true,
+        lua = true,
+        svelte = true,
+        css = true,
+        ['*'] = false
+      }
+    }
+  }
+}
