7b8666181fec349f97587f24aeda7ca5e1f32708
Ville Vartiainen <12277119+vvartiainen@users.noreply.github.com>
2024-03-25 13:32:06 +0200

    packages and lint file

---
+++ nvim/lua/plugins/lint.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        fish = { "fish" },
+        javascript = { "biomejs" },
+        typescript = { "biomejs" },
+        typescriptreact = { "biomejs" },
+        lua = { "luacheck" },
+        -- Use the "*" filetype to run linters on all filetypes.
+        -- ['*'] = { 'global linter' },
+        -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
+        -- ['_'] = { 'fallback linter' },
+      },
+    },
+  },
+}
