c1f6721e898ebd36dda1bef98ea7408cba88cd9b
Ville Vartiainen <vvarti@gmail.com>
2024-03-25 17:19:25 +0200

    minor ups

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,19 +3,25 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
     { "antosha417/nvim-lsp-file-operations", config = true },
+    { "j-hui/fidget.nvim", opts = {} },
+    { "folke/neodev.nvim", opts = {} },
   },
   opts = {
     servers = {
       tsserver = {
         on_attach = function(client)
-          -- biome is used for formatting
+          -- biome/prettier is used for formatting, conform.lua
           client.server_capabilities.documentFormattingProvider = false
         end,
       },
       biome = {},
       svelte = {},
       sourcekit = {},
+      gopls = {},
     },
   },
 }
