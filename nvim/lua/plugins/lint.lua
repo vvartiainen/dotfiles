fe3bb9e789e5b910ebd43a8964088ddfa4ab3fd2
Ville Vartiainen <vvarti@gmail.com>
2024-03-25 17:54:07 +0200

    add more linters

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -4,9 +4,9 @@ return {
     opts = {
       linters_by_ft = {
         fish = { "fish" },
-        javascript = { "biomejs" },
-        typescript = { "biomejs" },
-        typescriptreact = { "biomejs" },
+        javascript = { "biomejs", "quick-lint-js", "eslint" },
+        typescript = { "biomejs", "quick-lint-js", "eslint" },
+        typescriptreact = { "biomejs", "quick-lint-js", "eslint" },
         -- Use the "*" filetype to run linters on all filetypes.
         -- ['*'] = { 'global linter' },
         -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
