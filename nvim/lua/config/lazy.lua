e0eb5852fcca02923373a4d9bfadc78ac78c496b
Ville Vartiainen <vvarti@gmail.com>
2024-03-26 20:04:15 +0200

    should be redundant

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -9,8 +9,6 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.json" },
     -- import/override with your plugins
     { import = "plugins" },
   },
