9ae65b6d0d5a85f8c120e3f762241786004d9922
Ville Vartiainen <vvarti@gmail.com>
2024-03-27 19:01:35 +0200

    wezterm cfgs

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,24 +1,23 @@
 local wezterm = require("wezterm")
 
 return {
-  font_size = 12.0,
-  keys = {
-    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
-    { key = "LeftArrow",  mods = "OPT",      action = wezterm.action({ SendString = "\x1bb" }) },
-    -- Make Option-Right equivalent to Alt-f; forward-word
-    { key = "RightArrow", mods = "OPT",      action = wezterm.action({ SendString = "\x1bf" }) },
+	font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular" }),
+	font_size = 16.0,
+	keys = {
+		-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+		{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+		-- Make Option-Right equivalent to Alt-f; forward-word
+		{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
 
-    -- CMD + LEFT to move tabs
-    { key = "LeftArrow",  mods = "CMD",      action = wezterm.action({ ActivateTabRelative = -1 }) },
-    -- CMD + RIGHT to move tabs
-    { key = "RightArrow", mods = "CMD",      action = wezterm.action({ ActivateTabRelative = 1 }) },
-    -- Move tab to left with ctrl+cmd
-    { key = "LeftArrow",  mods = "CMD|CTRL", action = wezterm.action({ MoveTabRelative = -1 }) },
-    -- Move tab to right with ctrl+cmd
-    { key = "RightArrow", mods = "CMD|CTRL", action = wezterm.action({ MoveTabRelative = 1 }) },
-  },
-  color_scheme = "Catppuccin Mocha",
-  send_composed_key_when_left_alt_is_pressed = true,
-
-
+		-- CMD + LEFT to move tabs
+		{ key = "LeftArrow", mods = "CMD", action = wezterm.action({ ActivateTabRelative = -1 }) },
+		-- CMD + RIGHT to move tabs
+		{ key = "RightArrow", mods = "CMD", action = wezterm.action({ ActivateTabRelative = 1 }) },
+		-- Move tab to left with ctrl+cmd
+		{ key = "LeftArrow", mods = "CMD|CTRL", action = wezterm.action({ MoveTabRelative = -1 }) },
+		-- Move tab to right with ctrl+cmd
+		{ key = "RightArrow", mods = "CMD|CTRL", action = wezterm.action({ MoveTabRelative = 1 }) },
+	},
+	color_scheme = "Catppuccin Mocha",
+	send_composed_key_when_left_alt_is_pressed = true,
 }
