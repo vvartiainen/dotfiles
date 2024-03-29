295baf410cad4d03989c6678c2dbb23d6727ff08
Ville Vartiainen <vvarti@gmail.com>
2024-03-29 12:32:56 +0200

    keymap fixing for finnish layout

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,13 +2,21 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- € to $ for easier access
-vim.api.nvim_set_keymap("n", "€", "$", { noremap = true, silent = true })
-
--- copilot enable & disable
-vim.api.nvim_set_keymap("n", "<leader>cpe", ":Copilot enable<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>cpd", ":Copilot disable<CR>", { noremap = true, silent = true })
+-- € to $ for easier access on Finnish layout
+vim.api.nvim_set_keymap("n", "€", "$", { silent = true })
+vim.api.nvim_set_keymap("o", "€", "$", { silent = true })
+vim.api.nvim_set_keymap("x", "€", "$", { silent = true })
+vim.api.nvim_set_keymap("s", "€", "$", { silent = true })
+vim.api.nvim_set_keymap("v", "€", "$", { silent = true })
 
 -- map ö and ä to square brackets in all modes
 vim.api.nvim_set_keymap("n", "ö", "[", {})
 vim.api.nvim_set_keymap("n", "ä", "]", {})
+
+-- map å and ¨ to curly braces
+vim.api.nvim_set_keymap("n", "å", "{", {})
+vim.api.nvim_set_keymap("n", "¨", "}", {})
+
+-- copilot enable & disable
+vim.api.nvim_set_keymap("n", "<leader>cpe", ":Copilot enable<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>cpd", ":Copilot disable<CR>", { noremap = true, silent = true })
