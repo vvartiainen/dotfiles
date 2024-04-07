88bffa0c3ffd7c21216246e5c4868725fd6838fe
Ville Vartiainen <vvarti@gmail.com>
2024-04-07 03:23:14 +0300

    neotree to right

---
+++ nvim/lua/plugins/neotree.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      position = "right",
+      width = 30,
+      relative = "win",
+      row = 0,
+      col = 0,
+    },
+  },
+}
