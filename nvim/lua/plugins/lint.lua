112122c55ea88aec2e4610660426bfb98c7de0dc
Ville Vartiainen <vvarti@gmail.com>
2024-04-08 12:28:15 +0300

    sort keys, gitignore

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -3,15 +3,15 @@ return {
     "mfussenegger/nvim-lint",
     opts = {
       linters_by_ft = {
+        bash = { "shellcheck" },
         javascript = { "biomejs", "quick-lint-js", "eslint" },
+        markdown = { "markdownlint" },
+        sh = { "shellcheck" },
+        shell = { "shellcheck" },
+        sql = { "sqlfluff" },
         typescript = { "biomejs", "quick-lint-js", "eslint" },
         typescriptreact = { "biomejs", "quick-lint-js", "eslint" },
-        sql = { "sqlfluff" },
         zsh = { "zsh", "shellcheck" },
-        bash = { "shellcheck" },
-        shell = { "shellcheck" },
-        sh = { "shellcheck" },
-        markdown = { "markdownlint" },
         -- Use the "*" filetype to run linters on all filetypes.
         -- ['*'] = { 'global linter' },
         -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
