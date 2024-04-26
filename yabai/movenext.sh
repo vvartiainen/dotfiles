276118852fdd38427f99aa76a9c4e8ac779feef9
Ville Vartiainen <12277119+vvartiainen@users.noreply.github.com>
2024-04-26 15:19:52 +0300

    yabai

---
+++ yabai/movenext.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function get_window_id() {
+	yabai -m query --windows --window | jq '.id'
+}
+
+function move_window_prev_space() {
+	local window_id
+	window_id=$(get_window_id)
+	yabai -m window "$window_id" --space prev
+	yabai -m space --focus prev
+	yabai -m window --focus "$window_id"
+}
+
+function move_window_next_space() {
+	local window_id
+	window_id=$(get_window_id)
+	yabai -m window "$window_id" --space next
+	yabai -m space --focus next
+	yabai -m window --focus "$window_id"
+}
+
+move_window_next_space
