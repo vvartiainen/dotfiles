8f1e8d5f609f50c1484b2c97a4f2552d468ac6bd
Ville Vartiainen <vvarti@gmail.com>
2024-04-29 02:16:01 +0300

    colors

--- sketchybar/plugins/front_app.sh
+++ sketchybar/plugins/front_app.sh
@@ -8,5 +8,5 @@
 source "$CONFIG_DIR/colors.sh"
 
 if [ "$SENDER" = "front_app_switched" ]; then
-	sketchybar --set "$NAME" label="$INFO" icon="$("$CONFIG_DIR"/plugins/icon_map_fn.sh "$INFO")" icon.color="$SKY" label.color="$PEACH"
+	sketchybar --set "$NAME" label="$INFO" icon="$("$CONFIG_DIR"/plugins/icon_map_fn.sh "$INFO")" icon.color="$PEACH" label.color="$SKY"
 fi
