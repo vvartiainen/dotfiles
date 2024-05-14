d8a99491fddf117cbf8fe68f7ede772bdbd7c071
Ville Vartiainen <vvarti@gmail.com>
2024-05-14 17:45:55 +0300

    ups

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -29,7 +29,7 @@ return {
     cmp.setup({
       snippet = {
         expand = function(args)
-          require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+          require("luasnip").lsp_expand(args.body)
         end,
       },
       mapping = cmp.mapping.preset.insert({
