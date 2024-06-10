6734620d0bfb85f0136cee937ae7ae72a8804050
Ville Vartiainen <vvarti@gmail.com>
2024-06-10 13:28:33 +0300

    add jest

--- nvim/lua/plugins/vitest.lua
+++ nvim/lua/plugins/vitest.lua
@@ -2,10 +2,12 @@ return {
   "nvim-neotest/neotest",
   dependencies = {
     "marilari88/neotest-vitest",
+    "nvim-neotest/neotest-jest",
   },
   opts = {
     adapters = {
       ["neotest-vitest"] = {},
+      ["neotest-jest"] = {},
     },
   },
 }
