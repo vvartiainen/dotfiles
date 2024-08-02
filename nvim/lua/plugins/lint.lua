1f0e7211ec916ba83158551e882d90f1ca048d7f
Ville Vartiainen <12277119+vvartiainen@users.noreply.github.com>
2024-08-02 14:48:25 +0300

    biome provided by lazyvim already

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -3,13 +3,13 @@ return {
   opts = {
     linters_by_ft = {
       bash = { "shellcheck" },
-      javascript = { "biomejs", "quick-lint-js", "eslint" },
+      javascript = { "quick-lint-js", "eslint" },
       markdown = { "markdownlint" },
       sh = { "shellcheck" },
       shell = { "shellcheck" },
       sql = { "sqlfluff" },
-      typescript = { "biomejs", "quick-lint-js", "eslint" },
-      typescriptreact = { "biomejs", "quick-lint-js", "eslint" },
+      typescript = { "quick-lint-js", "eslint" },
+      typescriptreact = { "quick-lint-js", "eslint" },
       zsh = { "zsh", "shellcheck" },
       -- Use the "*" filetype to run linters on all filetypes.
       -- ['*'] = { 'global linter' },
