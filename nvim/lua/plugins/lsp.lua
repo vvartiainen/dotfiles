83148df9b4ca2730dd884137e92c0481bab6fe7a
Ville Vartiainen <12277119+vvartiainen@users.noreply.github.com>
2025-01-17 09:21:26 +0200

    cleanup

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,8 +5,6 @@ return {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    { "antosha417/nvim-lsp-file-operations", config = true },
-    { "j-hui/fidget.nvim", opts = {} },
   },
 
   opts = {
