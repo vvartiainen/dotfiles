54e7fafadb8d271f6f32981dcb88fea2c31e628c
Ville Vartiainen <vvarti@gmail.com>
2025-01-23 15:25:14 +0200

    blink

---
+++ nvim/lua/plugins/blink.lua
@@ -0,0 +1,16 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      list = {
+        selection = {
+          auto_insert = false,
+          preselect = false,
+        },
+      },
+    },
+    keymap = {
+      preset = "default",
+    },
+  },
+}
